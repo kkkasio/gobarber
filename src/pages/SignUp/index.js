3f2535e6f0465314c9d0f748e8900559ec594721
Kásio Eduardo <kasioeduardo13@gmail.com>
2019-09-26 06:32:47 -0300

    Validando formulário

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import { Container } from './styles';
+import * as Yup from 'yup';
 import { Form, Input } from '@rocketseat/unform';
 import logo from '~/assets/logo.svg';
 
@@ -9,10 +9,20 @@ export default function SignUp() {
     console.tron.log(event);
   }
 
+  const schema = Yup.object().shape({
+    email: Yup.string()
+      .email('Insira um e-mail válido')
+      .required('O E-mail é obrigatório'),
+    name: Yup.string().required('O nomé é obrigatório'),
+    password: Yup.string()
+      .required('A Senha é obrigatória')
+      .min(6, 'No mínimo 6 caracteres'),
+  });
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" type="email" placeholder="Nome Completo" />
         <Input name="email" type="email" placeholder="Preencha seu email" />
         <Input
