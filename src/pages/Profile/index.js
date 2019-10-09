9af741f0122fe2af18c24a2d93202f1bf67d9862
Kásio Eduardo <kasioeduardo13@gmail.com>
2019-10-09 17:28:06 -0300

    Página de perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <h1>Profile</h1>;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+  return (
+    <Container>
+      <Form initialData={profile}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" placeholder="Seu melhor email" />
+        <hr />
+        <Input
+          type="passowrd"
+          name="oldPassowrd"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="newPassword"
+          placeholder="Confirmação da senha"
+        />
+        <button type="submit">Atualizar Perfil</button>
+      </Form>
+      <button type="submit">Sair GoBarber</button>
+    </Container>
+  );
 }
