a515cdee2b2bf70233e19d7f19a0afb658db0b0b
Kasio Eduardo <kasioeduardo13@gmail.com>
2019-10-26 17:20:40 -0300

    Estilizações das rotas

--- mobile/src/pages/Profile/index.js
+++ mobile/src/pages/Profile/index.js
@@ -4,13 +4,13 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 import Background from '~/Components/Background';
 // import { Container } from './styles';
 
-export default function Dashboard() {
+export default function Profile() {
   return <Background />;
 }
 
-Dashboard.navigationOptions = {
-  tabBarLabel: 'Agendamentos',
+Profile.navigationOptions = {
+  tabBarLabe: 'Meu Perfil',
   tabBarIcon: ({ tintColor }) => (
-    <Icon name="event" size={20} color={tintColor} />
+    <Icon name="person" size={20} color={tintColor} />
   ),
 };
