ab6a23e060215e39ef449b6729a3a9747b8b9a69
Kasio Eduardo <kasioeduardo13@gmail.com>
2019-10-26 17:05:14 -0300

    Rota inicial

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -3,16 +3,17 @@ import '~/config/ReactotronConfig';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { StatusBar } from 'react-native';
-import Routes from './routes';
+import App from './App';
 import { store, persistor } from '~/store/';
+
 // import { Container } from './styles';
 
-export default function App() {
+export default function Index() {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-        <Routes />
+        <App />
       </PersistGate>
     </Provider>
   );
--- mobile/src/store/modules/auth/sagas.js
+++ mobile/src/store/modules/auth/sagas.js
@@ -1,5 +1,5 @@
 import { Alert } from 'react-native';
-import { takeLatest, call, put, all, delay } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
 import api from '~/services/api';
 
