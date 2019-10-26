0aad244cb492670e0cf6543af1f4953d5677bfbc
Kasio Eduardo <kasioeduardo13@gmail.com>
2019-10-26 07:05:52 -0300

    Página SignIn

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,18 +1,44 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image } from 'react-native';
 import Bacgrkound from '~/Components/Background';
-import Input from '~/Components/Input';
-import Button from '~/Components/Button';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.png';
+
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
 export default function SignIn() {
   return (
     <Bacgrkound>
-      <View>
-        <Input icon="face" placeholder="Digite seu nome" />
-        <Button>Entrar</Button>
-      </View>
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu email"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+          />
+
+          <SubmitButton onPress={() => {}}>Accessar</SubmitButton>
+        </Form>
+        <SignLink onPres={() => {}}>
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
     </Bacgrkound>
   );
 }
