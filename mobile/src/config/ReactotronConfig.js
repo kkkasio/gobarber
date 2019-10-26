ea76d296038fa1b3991421953e84e0fb87ce9be4
Kasio Eduardo <kasioeduardo13@gmail.com>
2019-10-26 09:08:20 -0300

    fix : Reactotron nova conexão sempre ao atualizar

--- mobile/src/config/ReactotronConfig.js
+++ mobile/src/config/ReactotronConfig.js
@@ -3,9 +3,12 @@ import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
 import reactotronSaga from 'reactotron-redux-saga';
 
+import AsyncStorage from '@react-native-community/async-storage';
+
 if (__DEV__) {
-  const tron = Reactotron.configure({ host: '192.168.0.9' })
+  const tron = Reactotron.configure({ name: 'MyApp' })
     .useReactNative()
+    .setAsyncStorageHandler(AsyncStorage)
     .use(reactotronRedux())
     .use(reactotronSaga())
     .connect();
